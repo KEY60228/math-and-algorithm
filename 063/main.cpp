b98fc4ba3bfb51fd1897b84ffc3695aa96412ccb
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2023-01-28 23:33:03 +0900

    063 - Move on Squares 2

---
+++ 063/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#define ll long long
+
+using namespace std;
+
+int main() {
+    ll n;
+    cin >> n;
+
+    if (n % 2 == 0) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
